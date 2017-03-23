04909c3e2bfca019e1d5436d5d03de881b5d84eb
Mathieu Bourque <skymatb@gmail.com>
2017-03-23 14:43:00 +0100

    pagination et affichage détails

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,14 @@
 $("document").ready(function() {
 
-var settings = {
+var people = {
   "async": true,
   "crossDomain": true,
-  "url": "https://api.themoviedb.org/3/discover/movie?page=1&include_video=false&include_adult=false&sort_by=popularity.asc&language=fr-FR&api_key=477dda0db0796b050196af446fe30383",
+  "url": "https://api.themoviedb.org/3/movie/"+idMovie+"/credits?api_key=477dda0db0796b050196af446fe30383",
   "method": "GET",
   "headers": {},
   "data": "{}"
 }
 
-$.ajax(settings).done(function (response) {
-  $
-});
+
 
 })
--- server.js
+++ server.js
@@ -13,10 +13,10 @@ mongoose.connect('mongodb://localhost/moviesapp' , function(err) {
 
 var listMovies = {};
 var pageG = 1;
-callDiscoverMovies(pageG,null);
-function callDiscoverMovies(page,renderPagination)
+callDiscoverMovies(pageG,null,null);
+function callDiscoverMovies(page,renderPage,id)
 {
-    console.log(page);
+    console.log("page: " + page, " -- renderPage: ", renderPage , " -- id: "+ id );
     if(listMovies[page] === undefined)
     {
         listMovies[page] = {};
@@ -37,54 +37,168 @@ function callDiscoverMovies(page,renderPagination)
                 listMovies.totalPages = body.total_pages;
                 body.results.forEach(function(element) {
                     listMovies[page][element.id] = element;
+                    listMovies[page][element.id].details = null;
                     console.log("ELEMENT ID - " + element.id);
-/*
-                   var settingsDetails = {
-                        "async": true,
-                        "crossDomain": true,
-                        "url": "https://api.themoviedb.org/3/movie/"+element.id+"?api_key=477dda0db0796b050196af446fe30383&language=fr-FR",
-                        "method": "GET",
-                        "headers": {},
-                        "data": "{}"
-                    }
-                    request(settingsDetails.url, function(error, response, body) {
-                        console.log("details - " + element.id);
-                        body = JSON.parse(body);
-                        if(body != undefined) {
-                            console.log("detail affectation - " + element.id);
-                            listMovies[page][element.id].details = body;
-                        }
-                    });
-*/
                 }, this);
             }
-            console.log(renderPagination);
-            if (renderPagination)
-                renderPagination();
+            console.log(renderPage);
+            if (renderPage)
+                if (!id)
+                    renderPage();
+                else
+                    callDiscoverMovieDetailsId(page,renderPage,id);           
         });
     }
     else
-        if (renderPagination)
-            renderPagination();
+        if (renderPage)
+            if (!id)
+                renderPage();
+            else
+                callDiscoverMovieDetailsId(page,renderPage,id);  
+}
+
+//fonction  pour appeler le WS de détails d'un film, avec son id
+function callDiscoverMovieDetailsId(page,renderPage,id){
+    console.log("DETAILS " ,listMovies[page][id].details != null);
+    //Si le détails a déjà été mis en mémoire, on rend la page sans rappeler le WS
+    if (listMovies[page][id].details != null){
+        if (renderPage)
+            renderPage(id);
+    }
+    else{
+        var settingsDetails = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://api.themoviedb.org/3/movie/"+id+"?api_key=477dda0db0796b050196af446fe30383&language=fr-FR",
+            "method": "GET",
+            "headers": {},
+            "data": "{}"
+        }
+        request(settingsDetails.url, function(error, response, body) {
+            console.log("details - " + id);
+            body = JSON.parse(body);
+            if(body != undefined && page) {
+                console.log("detail affectation - " + id);
+                listMovies[page][id].details = body;
+                formatAndSaveCategories(page,id);
+                listMovies[page][id].scenaristes = "";
+                listMovies[page][id].realisateurs = "";
+                listMovies[page][id].acteurs = "";
+
+                var people = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://api.themoviedb.org/3/movie/"+id+"/credits?api_key=477dda0db0796b050196af446fe30383",
+                    "method": "GET",
+                    "headers": {},
+                    "data": "{}"
+                }
+
+                request(people.url, function(error, response, body) {
+                    console.log("People - " + id);
+                    body = JSON.parse(body);
+                    if(body != undefined && page) {
+                        formatAndSaveRealisateurs(page,id,body);
+                        formatAndSaveScenaristes(page,id,body);
+                        formatAndSaveActeurs(page,id,body);
+                        if (renderPage)
+                            renderPage(id);
+                    }
+                    else if (renderPage)
+                        renderPage(id);
+                });
+            }
+        });
+    }
+}
+
+function formatAndSaveCategories(page,id){
+    categories = "";
+    for (var i = 0; i < listMovies[page][id].details.genres.length; i++) {
+        categories+=listMovies[page][id].details.genres[i].name;
+        if(i < listMovies[page][id].details.genres.length - 1)
+        categories+=" / ";
+    }
+    listMovies[page][id].categories = categories;
+}
+
+function formatAndSaveRealisateurs(page,id,body){
+    var realisateurs = "";
+    body.crew.forEach(function(element) {
+        if (element.job == "Director"){
+            if (realisateurs != "")
+                realisateurs += " / ";
+            realisateurs += element.name;
+        }
+    }, this);
+    listMovies[page][id].realisateurs = realisateurs;
+}
+
+function formatAndSaveScenaristes(page,id,body){
+    var scenaristes = "";
+    body.crew.forEach(function(element) {
+        if (element.job == "Story"){
+            if (scenaristes != "")
+                scenaristes += " / ";
+            scenaristes += element.name;
+        }
+    }, this);
+    listMovies[page][id].scenaristes = scenaristes;
+}
+
+function formatAndSaveActeurs(page,id,body){
+    var acteurs = "";
+    for (var i = 0; i < 6; i++) {
+        var element = body.cast[i];
+        acteurs += element.character + " ("+ element.name + ")";
+        if (i < 5)
+            acteurs += " / ";
+    }
+    listMovies[page][id].acteurs = acteurs;
+}
+
+function home(req,res){
+    if (req.query.page != null && parseInt(req.query.page, 10)){
+        pageG = parseInt(req.query.page, 10);
+    }
+    var renderPage = function () {
+        res.render('home', {listMovies,page : pageG});
+    }
+    callDiscoverMovies(pageG,renderPage,null);
 }
 
 app.get('/', function (req, res) {
-    console.log("Query page : " + req.query.page);
-    if (req.query.page != null && parseInt(req.query.page, 10)){
-        
-        pageG = parseInt(req.query.page, 10);
-        var renderPagination = function () {
-            res.render('home', {listMovies,page : pageG});
+    home(req,res);
+});
+
+app.get('/single', function (req, res) {
+    var renderPage = function (id) {
+        res.render('single', {listMovies,page : pageG,id});
+    }
+    if (req.query.id != null){
+        if(listMovies[pageG][req.query.id]!= undefined){
+            console.log("AVANT callDiscoverMovieDetailsId");
+            callDiscoverMovieDetailsId(pageG,renderPage,req.query.id);
+            console.log("APRES callDiscoverMovieDetailsId");
         }
-        console.log("Query page  CALLED: " + req.query.page);
-        callDiscoverMovies(pageG,renderPagination);
+        else
+        {
+            try {
+                    console.log("AVANT callDiscoverMovies");
+                    callDiscoverMovies(pageG,renderPage,req.query.id);
+                    console.log("APRES callDiscoverMovies");
+            }
+            catch (e) {
+                console.log("Exception",e);
+                res.render('home', {listMovies,page : pageG});
+            }
+
+        }
     }
 });
 
-
 app.get('/home', function (req, res) {
-  res.render('home', {
-  });
+    home(req,res);
 });
 
 app.get('/contact', function (req, res) {
@@ -97,10 +211,6 @@ app.get('/review', function (req, res) {
   });
 });
 
-app.get('/single', function (req, res) {
-  res.render('single', {
-  });
-});
 
 app.listen(80, function () {
   console.log("Server listening on port 80");
